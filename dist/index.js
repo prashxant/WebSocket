673e810e7cb01f413bbf65a450fa5978814e9d2a
prashxant <notxdead0@gmail.com>
2025-05-28 15:10:08 +0530

    set interval

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,10 @@
 "use strict";
-console.log("hi");
+Object.defineProperty(exports, "__esModule", { value: true });
+const ws_1 = require("ws");
+const wss = new ws_1.WebSocketServer({ port: 8080 });
+//event handler//
+wss.on("connection", function (socket) {
+    setInterval(() => {
+        socket.send("currnet price of solana is " + Math.random() * 10000);
+    }, 500);
+});
--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,9 @@ const wss = new WebSocketServer({port:8080});
 
 //event handler//
 wss.on("connection",function(socket){
-
-
+     
+    setInterval(()=>{
+        socket.send("currnet price of solana is " + Math.random()*10000)
+    },500)
+    
 })
