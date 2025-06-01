70afd56b41d4784dfd735b4fe127623d80699e22
prashxant <notxdead0@gmail.com>
2025-06-01 10:51:39 +0530

    PING PING CLIENT AND SERVER

--- be/dist/index.js
+++ be/dist/index.js
@@ -4,9 +4,6 @@ const ws_1 = require("ws");
 const wss = new ws_1.WebSocketServer({ port: 8080 });
 //event handler//
 wss.on("connection", function (socket) {
-    setInterval(() => {
-        socket.send("currnet price of solana is " + Math.random() * 10000);
-    }, 500);
     socket.on("message", (e) => {
         if (e.toString() === "ping") {
             socket.send("pong");
--- be/src/index.ts
+++ be/src/index.ts
@@ -5,9 +5,6 @@ const wss = new WebSocketServer({port:8080});
 //event handler//
 wss.on("connection",function(socket){
      
-    setInterval(()=>{
-        socket.send("currnet price of solana is " + Math.random()*10000)
-    },500)
   
     socket.on("message",(e) => {
         if(e.toString() === "ping"){
