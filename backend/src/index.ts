0caba7bec21a5e9e02dcddcff0881d33432c482c
prashxant <notxdead0@gmail.com>
2025-06-02 08:11:08 +0530

    moning 1

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,20 +1,23 @@
-import { WebSocketServer } from "ws";
+import { WebSocketServer,WebSocket } from "ws";
 const wss =  new WebSocketServer({port : 8080})
 
 
-let userCount = 0
+let userCount = 0;
+let allSocket: WebSocket[] = [];
 
 
 wss.on("connection",(socket)=>{
+    allSocket.push(socket)
     userCount = userCount+1
         console.log("User connected number #" + userCount)
 
 
         socket.on("message",(message)=>{
             console.log("message recived " + message.toString() )
-            setTimeout(() => {
-                socket.send(message.toString()+" sent from server")
-            }, 2000);
+           for (let i = 0 ; i < allSocket.length ; i++){
+            const s =  allSocket[i];
+            s.send(message.toString() + "sent form server")
+           }
         })
 
 })
