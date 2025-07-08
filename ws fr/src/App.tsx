0b4390c3c3abd24ae836e8d0844c48a3df1e2d4c
prashxant <notxdead0@gmail.com>
2025-07-08 21:00:45 +0530

    push push

--- ws fr/src/App.tsx
+++ ws fr/src/App.tsx
@@ -1,36 +1,36 @@
-
 import { useEffect, useRef, useState } from 'react'
 import './App.css'
 
+
 function App() {
-
-  const [socket , setSocket] = useState()
-  const inputRef = useRef()
-
-
- function sendMessage (){
-
-    if (!socket){
-        
-    }
-    const message= inputRef.current.value
-    socket.send(message)
- }
+  const [message,setMessage] = useState(["hi there"])
+  const inputRef = useRef
 
 useEffect(()=>{
-  const ws = new WebSocket("ws://localhost:8000")
-  setSocket(ws)
 
-  ws.onmessage = (ev) =>{
-      alert(ev.data)
-  }
+const ws = new WebSocket("http://localhost:3000")
+
+ws.onmessage = (event) =>{
+  setMessage(m => [...m,event.data])
+}
 },[])
 
-  return (
-    <div>
+ function sendMessage(){
 
-      <input ref={inputRef}type="text" placeholder='Message...' />
-      <button onClick={sendMessage}>send</button>
+  message = inputRef.current.value
+ }
+
+
+  return (
+
+    <div className='h-screen'>
+      <div className='h-[90vh] bg-slate-900 text-white'>
+        {message.map(message => <div>{message}</div>)}
+      </div>
+      <div className='w-full bg-white flex p-4'>
+        <input ref={inputRef}  type="text" name="" id="" />
+        <button onClick={sendMessage} className='bg-pink-500 rounded-md mx-4  text-white w-3/10'>Send Message</button>
+      </div>
     </div>
   )
 }
