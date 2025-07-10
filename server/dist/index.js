da26a9cea3a51caf420646a74b04b38f496a5d95
prashxant <notxdead0@gmail.com>
2025-07-10 10:43:42 +0530

    finished

--- server/dist/index.js
+++ server/dist/index.js
@@ -2,25 +2,24 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const ws_1 = require("ws");
 const wss = new ws_1.WebSocketServer({ port: 8080 });
-let allSocket = [];
+let users = [];
 wss.on("connection", (socket) => {
-    socket.on("message", (message) => {
-        // @ts-ignore
-        var _a;
-        const parsedMessage = JSON.parse(message);
-        if (parsedMessage.type == "join") {
-            allSocket.push({
-                socket,
-                room: parsedMessage.payload.roomId
+    socket.on("message", (data) => {
+        const message = JSON.parse(data.toString());
+        if (message.type === "join") {
+            users.push({ socket, room: message.payload.roomId });
+        }
+        if (message.type === "message") {
+            const roomUsers = users.filter((u) => u.room === message.payload.roomId);
+            roomUsers.forEach((u) => {
+                u.socket.send(JSON.stringify({
+                    type: "message",
+                    payload: { text: message.payload.text }
+                }));
             });
         }
-        if (parsedMessage.type == "chat") {
-            const currentUserRoom = (_a = allSocket.find((x) => x.socket == socket)) === null || _a === void 0 ? void 0 : _a.room;
-            allSocket.forEach((user) => {
-                if (user.room == currentUserRoom) {
-                    user.socket.send(parsedMessage.payload.message);
-                }
-            });
-        }
+        socket.on("close", () => {
+            users = users.filter((u) => u.socket !== socket);
+        });
     });
 });
